b6627dec35e5a52864156b2bcc2877a59b5e20a0
Marina Grigoreva <mgrigoreva12@gmail.com>
2022-08-05 15:31:24 +0300

    cli.php

--- cli.php
+++ cli.php
@@ -6,11 +6,22 @@ use Grimarina\Blog\{User, Post, Comment};
 
 $faker = Faker\Factory::create();
 
-$user = new User($faker->unique()->randomDigit, $faker->name);
-print($user);
+switch ($argv[1]) {
+    case "user":
+        $user = new User($faker->unique()->randomDigit, $faker->name);
+        print($user);
+        break;
 
-$post = new Post($faker->unique()->randomDigit, $faker->unique()->randomDigit, $faker->title(), $faker->text());
-print($post);
+    case "post":
+        $post = new Post($faker->unique()->randomDigit, $faker->unique()->randomDigit, $faker->text(), $faker->text());
+        print($post);
+        break;
 
-$comment = new Comment($faker->unique()->randomDigit, $faker->unique()->randomDigit, $faker->unique()->randomDigit, $faker->text());
-print($comment);
+    case "comment":
+        $comment = new Comment($faker->unique()->randomDigit, $faker->unique()->randomDigit, $faker->unique()->randomDigit, $faker->text());
+        print($comment);
+        break;
+
+    default:
+        echo "Not found argument";
+}
