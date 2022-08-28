495b511d51c4dc4d86853b768070bad8fc4ce118
Marina Grigoreva <mgrigoreva12@gmail.com>
2022-08-28 12:20:46 +0300

    Comments num option

--- src/Blog/Commands/FakeData/PopulateDB.php
+++ src/Blog/Commands/FakeData/PopulateDB.php
@@ -2,11 +2,10 @@
 
 namespace Grimarina\Blog_Project\Blog\Commands\FakeData;
 
-use Grimarina\Blog_Project\Blog\Post;
+use Grimarina\Blog_Project\Blog\{Comment, Post, User, UUID};
+use Grimarina\Blog_Project\Blog\Repositories\CommentsRepositories\CommentsRepositoryInterface;
 use Grimarina\Blog_Project\Blog\Repositories\PostsRepositories\PostsRepositoryInterface;
 use Grimarina\Blog_Project\Blog\Repositories\UsersRepositories\UsersRepositoryInterface;
-use Grimarina\Blog_Project\Blog\User;
-use Grimarina\Blog_Project\Blog\UUID;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -18,6 +17,7 @@ class PopulateDB extends Command
         private \Faker\Generator $faker,
         private UsersRepositoryInterface $usersRepository, 
         private PostsRepositoryInterface $postsRepository,
+        private CommentsRepositoryInterface $commentsRepository,
     ){ 
         parent::__construct();
     }
@@ -37,7 +37,13 @@ class PopulateDB extends Command
                 'posts-number',
                 'p',
                 InputOption::VALUE_OPTIONAL,
-                'Posts number'
+                'Posts number per user'
+            )
+            ->addOption(
+                'comments-number',
+                'c',
+                InputOption::VALUE_OPTIONAL,
+                'Comments number per post'
             );
     }
     
@@ -55,12 +61,23 @@ class PopulateDB extends Command
             $output->writeln('User created: ' . $user->getUsername());
         }
         
-        $postsNum = $input->getOption('posts-number') ?? 10;
+        $postsNum = $input->getOption('posts-number') ?? 1;
 
+        $posts = [];
         foreach ($users as $user) {
             for ($i = 0; $i < $postsNum & $i < 50; $i++) {
                 $post = $this->createFakePost($user); 
+                $posts[] = $post;
                 $output->writeln('Post created: ' . $post->getTitle()); 
+            }
+        }
+
+        $commentsNum = $input->getOption('comments-number') ?? 1;
+
+        foreach ($posts as $post) {
+            for ($i = 0; $i < $commentsNum & $i < 50; $i++) {
+                $comment = $this->createFakeComment($post);
+                $output->writeln('Comment created: ' . $comment->getUuid());
             }
         }
 
@@ -93,6 +110,20 @@ class PopulateDB extends Command
         $this->postsRepository->save($post); 
         
         return $post;
+    }
+
+    private function createFakeComment(Post $post): Comment 
+    {
+        $comment = new Comment( 
+            UUID::random(),
+            $post->getUuid(),
+            $post->getAuthor_uuid(),
+            $this->faker->realText
+        );
+        
+        $this->commentsRepository->save($comment); 
+        
+        return $comment;
     }
 
 }
